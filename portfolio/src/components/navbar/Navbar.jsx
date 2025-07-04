3e59fffb0a2fbe91b52f542e75f4a91833fe9de6
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-07-04 14:37:43 +0800

    updated

--- portfolio/src/components/navbar/Navbar.jsx
+++ portfolio/src/components/navbar/Navbar.jsx
@@ -4,8 +4,8 @@ import { logo } from "../../assets/index";
 function Navbar() {
   return (
     <div>
-      <div className="w-full h-3 mx-auto flex justify-between items-center">
-        <img src={logo} alt="logo" />
+      <div className="w-full h-20 mx-auto flex justify-between items-center">
+        <img src={logo} alt="logo" className="h-14" />
       </div>
       <div></div>
     </div>
