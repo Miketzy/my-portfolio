9cda4090a2e2df1398d9368a4a744ba7e845e93e
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-07-04 12:56:27 +0800

    updated

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,7 +1,11 @@
+import Navbar from "./components/navbar/Navbar";
+
 function App() {
   return (
     <>
-      <div>Portfolio</div>
+      <div className="app">
+        <Navbar />
+      </div>
     </>
   );
 }
