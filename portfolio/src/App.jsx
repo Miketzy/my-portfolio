2e225bec188341e8a2d61a30199da8223d82688a
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-07-04 13:07:07 +0800

    updated

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from "./components/navbar/Navbar";
 function App() {
   return (
     <>
-      <div className="w-full h-auto bg-gray-900">
+      <div className="w-full h-auto bg-gray-900 text-white">
         <Navbar />
       </div>
     </>
