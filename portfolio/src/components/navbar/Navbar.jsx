43c18fccbed4465127716bb55e96318dbaa22a51
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-07-04 14:59:20 +0800

    updated

--- portfolio/src/components/navbar/Navbar.jsx
+++ portfolio/src/components/navbar/Navbar.jsx
@@ -11,7 +11,12 @@ function Navbar() {
       <div>
         <ul className="flex items-center gap-10">
           {navLinksdata.map(({ _id, title, link }) => (
-            <li key={_id}>{title}</li>
+            <li
+              className="tracking-wide cursor-pointer hover:bg-red-500 duration-300"
+              key={_id}
+            >
+              {title}
+            </li>
           ))}
         </ul>
       </div>
