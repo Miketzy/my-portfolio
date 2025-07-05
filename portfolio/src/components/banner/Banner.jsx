e3ff10e9c4f3d225f4395ca54c2374c47705b577
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-07-05 20:40:45 +0800

    updated

--- portfolio/src/components/banner/Banner.jsx
+++ portfolio/src/components/banner/Banner.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Banner() {
   return (
-    <section className="w-full h-[800px] flex items-center border-b-[1px] border-b-black">
+    <section className="w-full h-[800px] flex  border-b-[1px] border-b-black">
       Banner
     </section>
   );
