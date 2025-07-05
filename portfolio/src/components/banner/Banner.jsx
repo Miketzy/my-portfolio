189aac494100a3d293eec6c33f2842739688c892
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-07-05 21:32:43 +0800

    updated

--- portfolio/src/components/banner/Banner.jsx
+++ portfolio/src/components/banner/Banner.jsx
@@ -7,7 +7,7 @@ function Banner() {
       className="w-full pt-10 pb-20 flex items-center border-b-[1px] border-b-black"
     >
       <div>
-        <div>
+        <div className="flex flex-col gap-3">
           <h4 className="text-lg font-normal">WELCOME TO MY WORLD</h4>
           <h1 className="text-6xl font-bold text-white">
             Hi, I'm{" "}
