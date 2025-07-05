9ef8367c27a486450e3a88da6530e86a814a645f
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-07-05 20:42:14 +0800

    updated

--- portfolio/src/components/features/Features.jsx
+++ portfolio/src/components/features/Features.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Features() {
-  return <div>Features</div>;
+  return (
+    <section className="w-full h-[800px] flex  border-b-[1px] border-b-black">
+      Banner
+    </section>
+  );
 }
 
 export default Features;
