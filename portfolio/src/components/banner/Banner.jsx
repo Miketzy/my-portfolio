b041338edcb54d4846fbb9e28faea787c4a33c13
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-07-07 09:18:45 +0800

    updated

--- portfolio/src/components/banner/Banner.jsx
+++ portfolio/src/components/banner/Banner.jsx
@@ -6,12 +6,12 @@ function Banner() {
   return (
     <section
       id="home"
-      className="w-full py-13 pb-13 flex items-center border-b-[1px] border-b-black"
+      className="w-full py-13 pb-13 flex items-center border-b border-b-black"
     >
       <LeftBanner />
-      <div className="w-1/2 flex justify0center items-center relative">
+      <div className="w-1/2 flex justify-center items-center relative">
         <img className="w-[500px] h-[680px] z-10" src={image} alt="" />
-        <div className="absolute bottom-0 w-[500px] bg-gradient-to-r from-[#1e2024] to-[#202327] shadow-2xl flex justify-center items-center"></div>
+        <div className="absolute bottom-0 w-[500px] h-[50px] bg-gradient-to-r from-[#1e2024] to-[#202327] rounded-b-lg shadow-2xl z-20 flex justify-center items-center"></div>
       </div>
     </section>
   );
