b3356c15485e51a1200f86a9cbe213e9a00724a0
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-07-07 10:33:20 +0800

    updated

--- portfolio/src/components/banner/Banner.jsx
+++ portfolio/src/components/banner/Banner.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import LeftBanner from "./LeftBanner";
-import { image } from "../../assets/index";
+import RightBanner from "./RightBanner";
 
 function Banner() {
   return (
@@ -9,10 +9,7 @@ function Banner() {
       className="w-full pt-1 pb-20 flex items-center border-b border-b-black"
     >
       <LeftBanner />
-      <div className="w-1/2 flex justify-center items-center relative">
-        <img className="w-[500px] h-[680px] z-10" src={image} alt="" />
-        <div className="absolute bottom-0 w-[500px] h-[500px] bg-gradient-to-r from-gray-800 to-gray-900 rounded-b-lg shadow-2xl flex justify-center items-center"></div>
-      </div>
+      <RightBanner />
     </section>
   );
 }
