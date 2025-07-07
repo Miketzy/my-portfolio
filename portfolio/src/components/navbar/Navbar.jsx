15ff76b60508c256ee121c97c997c7510b711d60
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-07-07 14:31:46 +0800

    updated

--- portfolio/src/components/navbar/Navbar.jsx
+++ portfolio/src/components/navbar/Navbar.jsx
@@ -6,7 +6,7 @@ import { navLinksdata } from "../../constant";
 function Navbar() {
   return (
     <div
-      className="w-full h-18 sticky top-0 z-50 bg-gray-900  flex justify-between items-center border-b-[1px] border-b-white-600
+      className="w-full h-18 sticky top-0 z-50 bg-gray-900 mx-auto flex justify-between items-center border-b-[1px] border-b-white-600 ml-50px
 "
     >
       <div>
