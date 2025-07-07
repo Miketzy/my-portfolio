f226b4be2a99d97de908499f7bb5225c454d7211
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-07-07 09:16:26 +0800

    updated

--- portfolio/src/components/banner/Banner.jsx
+++ portfolio/src/components/banner/Banner.jsx
@@ -11,8 +11,8 @@ function Banner() {
       <LeftBanner />
       <div className="w-1/2 flex justify0center items-center relative">
         <img className="w-[500px] h-[680px] z-10" src={image} alt="" />
+        <div className="absolute bottom-0 w-[500px] bg-gradient-to-r from-[#1e2024] to-[#202327] shadow-2xl flex justify-center items-center"></div>
       </div>
-      <div className="absolute bottom-0 w-[500px] bg-gradient-to-r from-[#1e2024] to-[#202327] shadow-2xl flex justify-center items-center"></div>
     </section>
   );
 }
