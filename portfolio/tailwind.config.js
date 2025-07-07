468b2530b24d7c1a50efe516304d116bbc6380d6
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-07-07 10:47:43 +0800

    updated

---
+++ portfolio/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+
+    boxShadow: {
+      shadowOne: "10px 10px 19px #1c1e22, -10px -10px 19px #262a23"
+    }
+  },
+  plugins: [],
+};
