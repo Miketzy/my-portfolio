8fc68273a0d5f710adde280fe3fc6dbd4f5857af
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-07-07 10:56:21 +0800

    updated

--- portfolio/src/components/banner/RightBanner.jsx
+++ portfolio/src/components/banner/RightBanner.jsx
@@ -5,7 +5,7 @@ function RightBanner() {
   return (
     <div className="w-1/2 flex justify-center items-center relative">
       <img className="w-[500px] h-[680px] z-10" src={image} alt="" />
-      <div className="absolute bottom-0 w-[500px] h-[500px] bg-gradient-to-r from-gray-800 to-gray-900 rounded-b-lg shadow-[10px_10px_19px_#1c1e22,-10px_-10px_19px_#262a23] flex justify-center items-center"></div>
+      <div className="absolute bottom-0 w-[500px] h-[500px] bg-gradient-to-r from-[#1e2024] to-[#202327] rounded-b-lg shadow-[10px_10px_19px_#1c1e22,-10px_-10px_19px_#262a23] flex justify-center items-center"></div>
     </div>
   );
 }
