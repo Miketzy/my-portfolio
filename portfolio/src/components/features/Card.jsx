f6ff182be0daa54c698e5f9c7ac0e1d5b2466d89
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-07-09 06:15:40 +0800

    updated

--- portfolio/src/components/features/Card.jsx
+++ portfolio/src/components/features/Card.jsx
@@ -5,7 +5,7 @@ function Card({ title, des, icon }) {
   return (
     <div className="w-full px-6 md:px-12 h-80 py-10 rounded-lg bg-gradient-to-r from-gray-800 to-gray-900 shadow-[...] flex items-center hover:bg-gradient-to-b hover:from-gray-700 hover:to-gray-800 transition-colors duration-300 group overflow-hidden">
       <div className="h-72 w-full overflow-hidden">
-        <div className="flex flex-col gap-8 h-full translate-y-20 group-hover:translate-y-0 transition-transform duration-500 ease-in-out">
+        <div className="flex flex-col gap-10 h-full translate-y-16 group-hover:translate-y-0 transition-transform duration-500 ease-in-out">
           <div className="w-10 h-8 flex flex-col justify-between">
             {/*<span className="text-5xl text-[#85DFE7]">{icon}</span>*/}
             <span className="w-full h-[2px] rounded-lg bg-[#85DFE7] inline-flex"></span>
