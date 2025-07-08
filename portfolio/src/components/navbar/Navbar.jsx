00ef8275b33ad623c32d3561617b2cdeff0f17f2
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-07-08 11:08:25 +0800

    updated

--- portfolio/src/components/navbar/Navbar.jsx
+++ portfolio/src/components/navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import { navLinksdata } from "../../constant";
 function Navbar() {
   return (
     <div className="w-full h-16 sticky top-0 bg-gray-900 z-50 flex justify-between items-center border-b border-b-white/60">
-      <div className="max-w-screen-xl mx-auto">
+      <div className="">
         <img src={logo} alt="logo" className="h-12" />
       </div>
       <div>
