d6223a13509263173d0d3887f46c2c04ed747ecf
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-07-09 04:57:43 +0800

    updated

--- portfolio/src/components/features/Features.jsx
+++ portfolio/src/components/features/Features.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Title from "../layouts/Title";
+import Card from "./Card";
 
 function Features() {
   return (
@@ -8,8 +9,8 @@ function Features() {
       className="w-full h-[800px] py-20 border-b-[1px] border-b-black"
     >
       <Title title="Features" des="What I do" />
-      <div className="w-1/2 px-12 h-80 py-10 rounded-lg bg-gradient-to-r from-gray-800 to-gray-900 shadow-[10px_10px_20px_rgba(0,0,0,0.5),_-10px_-10px_20px_rgba(255,255,255,0.05)] flex items-center hover:bg-gradient-to-b hover:from-gray-700 hover:to-gray-800 transition-colors duration-300">
-        {/* content here */}
+      <div>
+        <Card />
       </div>
     </section>
   );
