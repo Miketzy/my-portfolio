8655ad54ab6b8230c3d20070fb7aa4a9c6b17ea3
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-07-09 05:07:04 +0800

    updated

--- portfolio/src/components/features/Features.jsx
+++ portfolio/src/components/features/Features.jsx
@@ -9,13 +9,32 @@ function Features() {
       className="w-full  py-20 border-b-[1px] border-b-black"
     >
       <Title title="Features" des="What I do" />
+
       <div className="grid grid-cols-3 gap-20">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        <Card
+          title="Business Strategy"
+          des="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui aperiam quibusdam soluta culpa quod voluptate, esse rerum sed! Officia, nisi numquam non qui a eligendi fugit rem aliquid earum architecto!"
+        />
+        <Card
+          title="Business Strategy"
+          des="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui aperiam quibusdam soluta culpa quod voluptate, esse rerum sed! Officia, nisi numquam non qui a eligendi fugit rem aliquid earum architecto!"
+        />
+        <Card
+          title="Business Strategy"
+          des="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui aperiam quibusdam soluta culpa quod voluptate, esse rerum sed! Officia, nisi numquam non qui a eligendi fugit rem aliquid earum architecto!"
+        />
+        <Card
+          title="Business Strategy"
+          des="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui aperiam quibusdam soluta culpa quod voluptate, esse rerum sed! Officia, nisi numquam non qui a eligendi fugit rem aliquid earum architecto!"
+        />
+        <Card
+          title="Business Strategy"
+          des="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui aperiam quibusdam soluta culpa quod voluptate, esse rerum sed! Officia, nisi numquam non qui a eligendi fugit rem aliquid earum architecto!"
+        />
+        <Card
+          title="Business Strategy"
+          des="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui aperiam quibusdam soluta culpa quod voluptate, esse rerum sed! Officia, nisi numquam non qui a eligendi fugit rem aliquid earum architecto!"
+        />
       </div>
     </section>
   );
