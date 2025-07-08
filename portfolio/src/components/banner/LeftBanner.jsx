7af4f0c9040e14b0d78d7a88123ad623255be3e5
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-07-08 11:54:34 +0800

    updated

--- portfolio/src/components/banner/LeftBanner.jsx
+++ portfolio/src/components/banner/LeftBanner.jsx
@@ -46,6 +46,12 @@ function LeftBanner() {
         <h2 className="text-base uppercase mb-4">Find me in</h2>
         <div className="flex gap-4">
           <span className="bannerIcon">
+            <a
+              href="https://www.facebook.com/michael.john.garcia.margate"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="w-full h-full flex items-center justify-center"
+            ></a>
             <FaFacebookF />
           </span>
           <span className="bannerIcon">
