099354290c54aa5234b4c9673cea75e4dfde8940
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-07-08 11:13:22 +0800

    updated

--- portfolio/src/components/navbar/Navbar.jsx
+++ portfolio/src/components/navbar/Navbar.jsx
@@ -5,31 +5,29 @@ import { navLinksdata } from "../../constant";
 function Navbar() {
   return (
     <div className="w-full h-16 sticky top-0 bg-gray-900 z-50 flex justify-between items-center border-b border-b-white/60">
-      <div className="max-w-screen-xl mx-auto">
-        <div>
-          <img src={logo} alt="logo" className="h-12" />
-        </div>
-        <div>
-          <ul className="flex items-center gap-10">
-            {navLinksdata.map(({ _id, title, link }) => (
-              <li
-                className="text-base font-normal tracking-wide cursor-pointer hover:text-[#85DFE7] duration-300"
-                key={_id}
+      <div>
+        <img src={logo} alt="logo" className="h-12" />
+      </div>
+      <div>
+        <ul className="flex items-center gap-10">
+          {navLinksdata.map(({ _id, title, link }) => (
+            <li
+              className="text-base font-normal tracking-wide cursor-pointer hover:text-[#85DFE7] duration-300"
+              key={_id}
+            >
+              <Link
+                activeClass="active"
+                to={link}
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
               >
-                <Link
-                  activeClass="active"
-                  to={link}
-                  spy={true}
-                  smooth={true}
-                  offset={-70}
-                  duration={500}
-                >
-                  {title}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </div>
+                {title}
+              </Link>
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
