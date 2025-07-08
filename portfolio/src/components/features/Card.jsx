331cdf6fb82f6e89e6d0719ba2a483e0325fded8
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-07-09 05:27:40 +0800

    updated

--- portfolio/src/components/features/Card.jsx
+++ portfolio/src/components/features/Card.jsx
@@ -5,10 +5,15 @@ function Card({ title, des, icon }) {
     <div className="w-full px-12 h-80 py-10 rounded-lg bg-gradient-to-r from-gray-800 to-gray-900 shadow-[10px_10px_20px_rgba(0,0,0,0.5),_-10px_-10px_20px_rgba(255,255,255,0.05)] flex items-center hover:bg-gradient-to-b hover:from-gray-700 hover:to-gray-800 transition-colors duration-300">
       <div className="flex flex-col gap-8">
         <div>
-          <span className="txt-5xl text-[#85DFE7]">{icon}</span>
+          <span
+            className="text-5xl text-[#85DFE7]
+"
+          >
+            {icon}
+          </span>
         </div>
         <div>
-          <h2 className="txt-2xl font-bold">{title}</h2>
+          <h2 className="text-2xl font-bold">{title}</h2>
         </div>
       </div>
     </div>
