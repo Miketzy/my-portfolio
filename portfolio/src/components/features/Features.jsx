7488f4b66cca0443a97071cbcca4b0f648a38719
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-07-09 04:40:27 +0800

    updated

--- portfolio/src/components/features/Features.jsx
+++ portfolio/src/components/features/Features.jsx
@@ -8,6 +8,7 @@ function Features() {
       className="w-full h-[800px] py-20 flex  border-b-[1px] border-b-black"
     >
       <Title title="Features" des="What I do" />
+      <div className="w-full h-80  bg-yellow-500"></div>
     </section>
   );
 }
