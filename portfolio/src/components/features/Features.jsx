1ca08b7f59351a093d4ce7e38bc0333062f595d8
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-07-09 05:00:24 +0800

    updated

--- portfolio/src/components/features/Features.jsx
+++ portfolio/src/components/features/Features.jsx
@@ -9,7 +9,12 @@ function Features() {
       className="w-full h-[800px] py-20 border-b-[1px] border-b-black"
     >
       <Title title="Features" des="What I do" />
-      <div>
+      <div className="grid grid-col-3 gap-20">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
         <Card />
       </div>
     </section>
