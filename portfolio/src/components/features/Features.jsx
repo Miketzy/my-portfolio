ba0d0b955a97deb93a2b9e46b3e707be0d958bf6
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-07-09 04:53:04 +0800

    updated

--- portfolio/src/components/features/Features.jsx
+++ portfolio/src/components/features/Features.jsx
@@ -8,7 +8,9 @@ function Features() {
       className="w-full h-[800px] py-20 border-b-[1px] border-b-black"
     >
       <Title title="Features" des="What I do" />
-      <div className="w-full px-12 h-80 py-10 rounded-lg bg-gradient-to-r from-bg-gray-800 to-bg-gray-900 shadow-[10px_10px_20px_rgba(0,0,0,0.5),_-10px_-10px_20px_rgba(255,255,255,0.05)] flex items-center hover:bg-gradient-to-b hover:from:bg-gray-800 hover:to-bg-gray-750 transition-color duration-100"></div>
+      <div className="w-1/2 px-12 h-80 py-10 rounded-lg bg-gradient-to-r from-gray-800 to-gray-900 shadow-[10px_10px_20px_rgba(0,0,0,0.5),_-10px_-10px_20px_rgba(255,255,255,0.05)] flex items-center hover:bg-gradient-to-b hover:from-gray-700 hover:to-gray-800 transition-colors duration-300">
+        {/* content here */}
+      </div>
     </section>
   );
 }
