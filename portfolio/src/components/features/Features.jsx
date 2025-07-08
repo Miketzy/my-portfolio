b781b40ac99e2299f93991aa865facaa17dab724
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-07-09 04:27:42 +0800

    updated

--- portfolio/src/components/features/Features.jsx
+++ portfolio/src/components/features/Features.jsx
@@ -6,13 +6,13 @@ function Features() {
       id="features"
       className="w-full h-[800px] py-20 flex  border-b-[1px] border-b-black"
     >
-     <div className="flex flex-col gap-4">
-       <h3 className="text-sm uppercase font-light text-[#85DFE7] tracking-wide">
-        Features
-      </h3>
-      <h1 className="text-5xl font-bold capitalize">What I do</h1>
+      <div className="flex flex-col gap-4">
+        <h3 className="text-sm uppercase font-light text-[#85DFE7] tracking-wide">
+          Features
+        </h3>
+        <h1 className="text-5xl font-bold capitalize">What I do</h1>
+      </div>
     </section>
-     </div>
   );
 }
 
