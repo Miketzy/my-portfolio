7c132e5bccfbb791ce99d68e415f5f82c764e5cc
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-07-09 11:39:22 +0800

    updated

--- portfolio/src/components/features/Features.jsx
+++ portfolio/src/components/features/Features.jsx
@@ -20,7 +20,7 @@ function Features() {
     >
       <Title title="Features" des="What I do" />
 
-      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-20 ">
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-20 p-4">
         <Card
           title="Clean HTML Structure"
           des="I make simple and organized web pages using html so they are easy to read and understand."
