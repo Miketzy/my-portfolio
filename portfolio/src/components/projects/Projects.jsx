1a61c0017bd7e38b8f5c0f526d7cf91d203de665
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-07-10 22:29:02 +0800

    updated

---
+++ portfolio/src/components/projects/Projects.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Projects() {
+  return (
+    <section
+      id="projects"
+      className="w-full pt-20 pb-20 border-b-[1px] border-b-black p-4"
+    >
+      <div>
+        <h1>Projects</h1>
+      </div>
+    </section>
+  );
+}
+
+export default Projects;
