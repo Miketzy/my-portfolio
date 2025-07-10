41a64642a6cefa04d6d388f462052c52257359fd
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-07-10 13:13:44 +0800

    updated

--- portfolio/src/components/banner/Banner.jsx
+++ portfolio/src/components/banner/Banner.jsx
@@ -6,7 +6,7 @@ function Banner() {
   return (
     <section
       id="home"
-      className="w-full pt-20 pb-20 md:pt-1 md:pb-20 px-4 flex flex-col md:flex-row items-start md:items-center border-b border-b-black"
+      className="w-full pt-20 pb-20 md:pt-20 md:pb-20 px-4 flex flex-col md:flex-row items-start md:items-center border-b border-b-black"
     >
       <LeftBanner />
       <RightBanner />
