cba6b55e5e6f8b5d17fac2a25273d96d143a660d
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-07-10 23:17:25 +0800

    updated

--- portfolio/src/components/projects/Projects.jsx
+++ portfolio/src/components/projects/Projects.jsx
@@ -4,7 +4,7 @@ function Projects() {
   return (
     <section
       id="projects"
-      className="w-full pt-20 pb-20 border-b-[1px] border-b-black p-4"
+      className="w-full h-[800px]pt-20 pb-20 border-b-[1px] border-b-black p-4"
     >
       <div>
         <h1>Projects</h1>
