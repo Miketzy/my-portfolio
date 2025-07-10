32ac045db6e817e2de2df95db8ebfc426dcdcb31
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-07-10 12:57:23 +0800

    updated

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -6,10 +6,12 @@ function App() {
   return (
     <>
       <div className="w-full h-auto bg-gray-900 text-white">
-        <div className="max-w-screen-xl mx-auto">
-          <Navbar />
-          <Banner />
-          <Features />
+        <Navbar />
+        <div className="overflow-x-hidden">
+          <div className="max-w-screen-xl mx-auto">
+            <Banner />
+            <Features />
+          </div>
         </div>
       </div>
     </>
