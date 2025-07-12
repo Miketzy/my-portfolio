2231ba42b919b5463567ce11a8cdc0ef5028c4fa
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-07-12 08:32:28 +0800

    updated

--- portfolio/src/components/projects/Projects.jsx
+++ portfolio/src/components/projects/Projects.jsx
@@ -6,10 +6,12 @@ function Projects() {
       id="projects"
       className="w-full  pt-20 pb-20 border-b-[1px] border-b-black p-4"
     >
-      <Title
-        title="VISIT MY PORTFOLIO AND KEEP YOUR FEEDBACK"
-        des="My Projects"
-      />
+      <div className="flex justify-center">
+        <Title
+          title="VISIT MY PORTFOLIO AND KEEP YOUR FEEDBACK"
+          des="My Projects"
+        />
+      </div>
     </section>
   );
 }
