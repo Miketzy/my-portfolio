4851dd07fc4992c7de7729b6fbde83fbde10a9ee
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-07-12 08:55:34 +0800

    updated

--- portfolio/src/components/projects/ProjectCard.jsx
+++ portfolio/src/components/projects/ProjectCard.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function ProjectCard() {
-  return <div className="">projectCard</div>;
+  return (
+    <div className="w-full px-12 h-auto py-10 rounded-lg bg-gradient-to-r from-gray-800 to-gray-900 shadow-[10px_10px_20px_rgba(0,0,0,0.5),_-10px_-10px_20px_rgba(255,255,255,0.05)] hover:bg-gradient-to-b hover:from-gray-700 hover:to-gray-800 transition-colors duration-300">
+      Project Cards
+    </div>
+  );
 }
 
 export default ProjectCard;
