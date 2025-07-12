9ab41c0da063c1e6e67a0a05bd924aad5e023ccb
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-07-12 09:48:42 +0800

    updated

---
+++ portfolio/src/components/resume/Resume.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Resume() {
+  return (
+    <section
+      id="projects"
+      className="w-full h-[800px] pt-20 pb-20 border-b-[1px] border-b-black p-4"
+    >
+      <div>Resume</div>
+    </section>
+  );
+}
+
+export default Resume;
