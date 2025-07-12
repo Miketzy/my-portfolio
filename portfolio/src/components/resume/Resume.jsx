7710a2784c7156bb0873d0c3d8ce58c8c44153c3
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-07-12 09:49:07 +0800

    updated

--- portfolio/src/components/resume/Resume.jsx
+++ portfolio/src/components/resume/Resume.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Resume() {
   return (
     <section
-      id="projects"
+      id="resume"
       className="w-full h-[800px] pt-20 pb-20 border-b-[1px] border-b-black p-4"
     >
       <div>Resume</div>
