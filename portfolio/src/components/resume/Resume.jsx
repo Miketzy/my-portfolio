92ce015072a5ceb3734b84a5c7388f9d3f85b5b4
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-07-14 09:41:05 +0800

    updated

--- portfolio/src/components/resume/Resume.jsx
+++ portfolio/src/components/resume/Resume.jsx
@@ -5,12 +5,12 @@ function Resume() {
   return (
     <section
       id="resume"
-      className="w-full h-[800px] pt-20 pb-20 border-b-[1px] border-b-black p-4"
+      className="w-full pt-20 pb-20 border-b-[1px] border-b-black p-4"
     >
       <div className="flex justify-center items-center text-center">
         <Title title="No Experience" des="My Resume" />
       </div>
-      <div className="flex flex-col w-full max-w-[350px] mx-auto xl:mx-0 gap-6">
+      <div className="">
         <ul>
           <li className="resumeLi">Education</li>
           <li>Skills</li>
