4e3d5dc116db4fdabaa1fabafc07fd537a87d153
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-07-14 09:12:40 +0800

    updated

--- portfolio/src/components/resume/Resume.jsx
+++ portfolio/src/components/resume/Resume.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Title from "../layouts/Title";
 
 function Resume() {
   return (
@@ -6,7 +7,9 @@ function Resume() {
       id="resume"
       className="w-full h-[800px] pt-20 pb-20 border-b-[1px] border-b-black p-4"
     >
-      <div>Resume</div>
+      <div className="flex justify-center items-center text-center">
+        <Title title="No Experience" des="My Resume" />
+      </div>
     </section>
   );
 }
