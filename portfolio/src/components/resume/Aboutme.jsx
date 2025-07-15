eee11859be4a15f6c4f7ca844761efef3853c8ea
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-07-15 09:59:12 +0800

    updated

--- portfolio/src/components/resume/Aboutme.jsx
+++ portfolio/src/components/resume/Aboutme.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Aboutme() {
   return (
-    <div className="flex flex-col gap-10 w-full lg:w-[500px]">
+    <div className="flex flex-col gap-10 w-full lg:w-[800px]">
       {/* Top Section: About text */}
       <div className="flex flex-col gap-4">
         <h1 className="text-4xl text-[#85DFE7] font-bold">About Me</h1>
