1f205fd63107d86cc975972d53383a79f609983e
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-07-15 09:18:03 +0800

    updated

--- portfolio/src/components/resume/Aboutme.jsx
+++ portfolio/src/components/resume/Aboutme.jsx
@@ -2,8 +2,10 @@ import React from "react";
 
 function Aboutme() {
   return (
-    <div>
-      <div>Aboutme</div>
+    <div className="w-full lg:w-[350px]">
+      <div className="flex flex-col gap-10">
+        <h1 className="text-4xl text:[#85DFE7] font-bold">About me</h1>
+      </div>
     </div>
   );
 }
