02cccb053e52aff2959843182feac6e0f54f11e9
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-07-19 07:15:29 +0800

    updated

--- portfolio/src/components/resume/Education.jsx
+++ portfolio/src/components/resume/Education.jsx
@@ -23,17 +23,36 @@ function Education() {
         <EducationCard
           year="2025"
           title="Bachelor of Science in Information Technology"
-          des="Graduated from Davao Oriental State University with a strong foundation in software and web development. I led the development of our capstone project, a full-stack web application focused on species management and admin approval systems using React.js and Tailwind CSS."
+          des={
+            <>
+              <ul className="list-disc list-inside text-sm">
+                <li>Graduated from Davao Oriental State University</li>
+              </ul>
+            </>
+          }
+        />
+
+        <EducationCard
+          year="2025"
+          title="Bachelor of Science in Information Technology"
+          des={
+            <>
+              <ul className="list-disc list-inside text-sm">
+                <li>Graduated from Davao Oriental State University</li>
+              </ul>
+            </>
+          }
         />
         <EducationCard
           year="2025"
           title="Bachelor of Science in Information Technology"
-          des="Graduated from Davao Oriental State University with a strong foundation in software and web development. I led the development of our capstone project, a full-stack web application focused on species management and admin approval systems using React.js and Tailwind CSS."
-        />
-        <EducationCard
-          year="2025"
-          title="Bachelor of Science in Information Technology"
-          des="Graduated from Davao Oriental State University with a strong foundation in software and web development. I led the development of our capstone project, a full-stack web application focused on species management and admin approval systems using React.js and Tailwind CSS."
+          des={
+            <>
+              <ul className="list-disc list-inside text-sm">
+                <li>Graduated from Davao Oriental State University</li>
+              </ul>
+            </>
+          }
         />
       </div>
     </div>
