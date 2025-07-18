2daf692cef41a57e0dff44b01de8972098c93bae
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-07-19 07:08:52 +0800

    updated

--- portfolio/src/components/resume/Education.jsx
+++ portfolio/src/components/resume/Education.jsx
@@ -3,7 +3,7 @@ import EducationCard from "./EducationCard";
 
 function Education() {
   return (
-    <div className="flex flex-col gap-10 w-full lg:w-[600px]">
+    <div className="flex flex-col gap-10 w-full lg:w-[700px]">
       {/* Top Section: About text */}
       <div className="flex flex-col gap-4">
         <h1 className="text-4xl text-[#85DFE7] font-bold">My Education</h1>
--- portfolio/src/components/resume/EducationCard.jsx
+++ portfolio/src/components/resume/EducationCard.jsx
@@ -2,14 +2,16 @@ import React from "react";
 
 function EducationCard({ year, title, des }) {
   return (
-    <div className="w-full px-6 md:px-12 py-10 rounded-lg bg-gradient-to-r from-gray-800 to-gray-900 shadow-[10px_10px_20px_rgba(0,0,0,0.5),_-10px_-10px_20px_rgba(255,255,255,0.05)] flex items-center hover:bg-gradient-to-b hover:from-gray-700 hover:to-gray-800 transition-colors duration-300 group overflow-hidden min-h-[280px]">
+    <div className="w-full px-6 md:px-12 py-10 rounded-lg bg-gradient-to-r from-gray-800 to-gray-900 shadow-[10px_10px_20px_rgba(0,0,0,0.5),_-10px_-10px_20px_rgba(255,255,255,0.05)] flex items-center hover:bg-gradient-to-b hover:from-gray-700 hover:to-gray-800 transition-colors duration-300 group overflow-hidden min-h-[320px]">
       <div className="w-full">
-        <div className="flex flex-col gap-6 transition-transform duration-500 ease-in-out">
-          <h2 className="text-2xl font-bold text-white">{year}</h2>
-          <h2 className="text-2xl font-bold text-white leading-snug">
+        <div className="flex flex-col gap-4 transition-transform duration-500 ease-in-out">
+          <h2 className="text-xl font-semibold text-white">{year}</h2>
+          <h2 className="text-xl font-semibold text-white leading-snug tracking-wide">
             {title}
           </h2>
-          <p className="text-base text-gray-300">{des}</p>
+          <p className="text-sm text-gray-300 leading-relaxed tracking-normal">
+            {des}
+          </p>
         </div>
       </div>
     </div>
