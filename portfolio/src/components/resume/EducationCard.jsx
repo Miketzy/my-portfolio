4795e5e01a7ac520160bad17d5ea35709308359a
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-07-19 07:21:02 +0800

    updated

--- portfolio/src/components/resume/EducationCard.jsx
+++ portfolio/src/components/resume/EducationCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function EducationCard({ year, title, des }) {
   return (
-    <div className="w-full px-6 md:px-12 py-10 rounded-lg bg-gradient-to-r from-gray-800 to-gray-900 shadow-[10px_10px_20px_rgba(0,0,0,0.5),_-10px_-10px_20px_rgba(255,255,255,0.05)] flex items-center hover:bg-gradient-to-b hover:from-gray-700 hover:to-gray-800 transition-colors duration-300 group overflow-hidden min-h-[320px]">
+    <div className="w-full px-6 md:px-12 py-10 rounded-lg bg-gradient-to-r from-gray-800 to-gray-900 shadow-[10px_10px_20px_rgba(0,0,0,0.5),_-10px_-10px_20px_rgba(255,255,255,0.05)] flex items-center hover:bg-gradient-to-b hover:from-gray-700 hover:to-gray-800 transition-colors duration-300 group overflow-hidden min-h-[180px]">
       <div className="w-full">
         <div className="flex flex-col gap-4 transition-transform duration-500 ease-in-out">
           <h2 className="text-xl font-semibold text-white">{year}</h2>
