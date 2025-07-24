52eeedfb6ec2766878901cd08ab68a34ed57be85
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-07-25 06:59:43 +0800

    updated

--- portfolio/src/components/resume/Skills.jsx
+++ portfolio/src/components/resume/Skills.jsx
@@ -2,9 +2,17 @@ import React from "react";
 
 function Skills() {
   return (
-    <div>
-      <div>Skills</div>
-    </div>
+    <section id="projects" className="w-full p-4 gap-10 lg:w-[700] ">
+      <div className="flex flex-col gap-4">
+        <h1 className="text-4xl text-[#85DFE7] font-bold">My Skills</h1>
+        <p className="text-base leading-relax text-white">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+          accusamus mollitia iusto ipsum! Velit doloremque repudiandae rem
+          tenetur facere aliquam quaerat magnam exercitationem, delectus autem
+          inventore, voluptate, perspiciatis molestiae blanditiis?
+        </p>
+      </div>
+    </section>
   );
 }
 
