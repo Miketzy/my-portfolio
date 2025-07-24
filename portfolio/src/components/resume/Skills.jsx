b924169f34efadb7aba60675dfb1bc6df1588753
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-07-25 07:00:39 +0800

    updated

--- portfolio/src/components/resume/Skills.jsx
+++ portfolio/src/components/resume/Skills.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Skills() {
   return (
-    <section id="projects" className="w-full p-4 gap-10 lg:w-[700] ">
+    <section id="projects" className="w-full p-4  lg:w-[700] ">
       <div className="flex flex-col gap-4">
         <h1 className="text-4xl text-[#85DFE7] font-bold">My Skills</h1>
         <p className="text-base leading-relax text-white">
