ad3bea2f1f22d361bb22a080e9ee702faf38b727
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-07-25 07:01:56 +0800

    updated

--- portfolio/src/components/resume/Skills.jsx
+++ portfolio/src/components/resume/Skills.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Skills() {
   return (
-    <section id="projects" className="w-full p-4  lg:w-[700] ">
+    <div className="flex p-4 flex-col gap-10 w-full lg:w-[700px]">
       <div className="flex flex-col gap-4">
         <h1 className="text-4xl text-[#85DFE7] font-bold">My Skills</h1>
         <p className="text-base leading-relax text-white">
@@ -12,7 +12,7 @@ function Skills() {
           inventore, voluptate, perspiciatis molestiae blanditiis?
         </p>
       </div>
-    </section>
+    </div>
   );
 }
 
