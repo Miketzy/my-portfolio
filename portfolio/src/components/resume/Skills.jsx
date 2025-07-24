df1e4addc66be036c85b20b8e768b7a6535d9e30
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-07-25 07:49:50 +0800

    updated

--- portfolio/src/components/resume/Skills.jsx
+++ portfolio/src/components/resume/Skills.jsx
@@ -5,7 +5,7 @@ function Skills() {
     <div className="flex p-4  flex-col gap-10 w-full lg:w-[700px]">
       <div className="flex flex-col gap-4">
         <h1 className="text-4xl text-[#85DFE7] font-bold">My Skills</h1>
-        <p className="text-base leading-relax text-white">
+        <p className="text-base leading-relax text-white mb-6">
           I am a fresh graduate with a Bachelor of Science in Information
           Technology. During my studies, I gained hands-on experience in
           frontend development using React.js and Tailwind CSS. For our capstone
