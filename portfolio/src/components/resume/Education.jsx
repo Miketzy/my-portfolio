c19645baf27f95bd75b830041424ab003955574b
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-07-25 07:24:10 +0800

    updated

--- portfolio/src/components/resume/Education.jsx
+++ portfolio/src/components/resume/Education.jsx
@@ -4,7 +4,7 @@ import { projectOne } from "../../assets";
 
 function Education() {
   return (
-    <div className="flex pt-[50px] flex-col gap-10 w-full lg:w-[700px]">
+    <div className="flex pt-[30px] flex-col gap-10 w-full lg:w-[700px]">
       {/* Top Section: About text */}
       <div className="flex flex-col gap-4">
         <h1 className="text-4xl text-[#85DFE7] font-bold">My Education</h1>
