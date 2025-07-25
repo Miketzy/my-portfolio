9ccc99cd9c38321a9480fc77a7817b6ffab327b2
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-07-25 08:14:49 +0800

    updated

--- portfolio/src/components/resume/SkillsCard.jsx
+++ portfolio/src/components/resume/SkillsCard.jsx
@@ -2,8 +2,12 @@ import React from "react";
 
 function SkillsCard({ images }) {
   return (
-    <div className="bg-[#1f2937] w-[20px] h-[20px] rounded-lg shadow-md flex items-center justify-center overflow-hidden">
-      <img src={images} alt="Skill" className="w-full h-full object-contain" />
+    <div className="bg-[#1f2937] w-[50px] h-[50px] rounded-lg shadow-md flex items-center justify-center overflow-hidden">
+      <img
+        src={images}
+        alt="Skill"
+        className="w-[30px] h-[30px] object-contain"
+      />
     </div>
   );
 }
