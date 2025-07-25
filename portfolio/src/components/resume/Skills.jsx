2fd570093d1589cdb9d34b81dc537d43f36cd60c
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-07-25 08:01:19 +0800

    updated

--- portfolio/src/components/resume/Skills.jsx
+++ portfolio/src/components/resume/Skills.jsx
@@ -6,14 +6,10 @@ function Skills() {
       <div className="flex flex-col gap-4">
         <h1 className="text-4xl text-[#85DFE7] font-bold">My Skills</h1>
         <p className="text-base leading-relax text-white mb-6">
-          I am a fresh graduate with a Bachelor of Science in Information
-          Technology. During my studies, I gained hands-on experience in
-          frontend development using React.js and Tailwind CSS. For our capstone
-          project, I took the lead in building a complete web-based system from
-          scratch, applying modern web development practices and responsive
-          design principles. This project strengthened my technical skills and
-          taught me how to work effectively both independently and as part of a
-          team.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus
+          similique molestiae magnam facilis vero deserunt ratione delectus
+          eaque mollitia ut earum quibusdam, tempore, aliquid ipsam perferendis
+          aut nisi rerum laboriosam!
         </p>
       </div>
     </div>
