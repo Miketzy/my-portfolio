e7bade71c0730a6cdd8433e6a5a7cdaf0f2c634a
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-07-27 19:08:54 +0800

    updated

--- portfolio/src/assets/index.jsx
+++ portfolio/src/assets/index.jsx
@@ -1,8 +1,8 @@
 import logo from "./images/mike4.png";
 import image from "./images/banner.png";
 import projectOne from "./images/Paper Instagram Logo - Margaret Scrinkl.jfif";
-import css from "./images/css.jpg";
-import html from "./images/html.jpg";
+import css from "./images/icons8-css-logo-480.png";
+import html from "./images/icons8-html-5-480.png";
 import reactjs from "./images/Reactjs.jpg";
 import tailwindcss from "./images/tailwindcss.jpg";
 
--- portfolio/src/components/resume/SkillsCard.jsx
+++ portfolio/src/components/resume/SkillsCard.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 
-function SkillsCard({ images, src }) {
+function SkillsCard({ images }) {
   return (
     <div className="bg-[#1f2937] w-[50px] h-[50px] rounded-lg shadow-md flex items-center justify-center overflow-hidden">
-      <img src={src} alt="Skill" className="w-[30px] h-[30px] object-contain" />
+      <img
+        src={images}
+        alt="Skill"
+        className="w-[30px] h-[30px] object-contain"
+      />
     </div>
   );
 }
