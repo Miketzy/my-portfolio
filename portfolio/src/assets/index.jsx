c3eb96745d4f49a9c65780a32c57f099d7c42547
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-07-27 20:14:15 +0800

    updated

--- portfolio/src/assets/index.jsx
+++ portfolio/src/assets/index.jsx
@@ -3,7 +3,7 @@ import image from "./images/banner.png";
 import projectOne from "./images/Paper Instagram Logo - Margaret Scrinkl.jfif";
 import css from "./images/icons8-css-logo-480.png";
 import html from "./images/icons8-html-5-480.png";
-import reactjs from "./images/Reactjs.jpg";
-import tailwindcss from "./images/tailwindcss.jpg";
+import reactjs from "./images/icons8-react-400.png";
+import tailwindcss from "./images/icons8-tailwindcss-480.png";
 
 export { logo, image, projectOne, css, html, reactjs, tailwindcss };
