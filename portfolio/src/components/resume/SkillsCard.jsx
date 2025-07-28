bf93d19a04cff827d6d4766e6781ff66dfb5f2ef
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-07-28 20:13:34 +0800

    updated

--- portfolio/src/components/resume/SkillsCard.jsx
+++ portfolio/src/components/resume/SkillsCard.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 function SkillsCard({ images }) {
   return (
-    <div className="bg-[#1f2937] w-[50px] h-[50px] rounded-lg shadow-md flex items-center justify-center overflow-hidden">
+    <div className="bg-[#1f2937] w-[80px] h-[80px] rounded-lg shadow-md flex items-center justify-center overflow-hidden">
       <img
         src={images}
         alt="Skill"
-        className="w-[30px] h-[30px] object-contain"
+        className="w-[50px] h-[50px] object-contain"
       />
     </div>
   );
