37127edc67072fabb5347022aa904202f9e9fcb5
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-07-29 17:36:35 +0800

    updated

--- portfolio/src/components/testimonial/Testimonial.jsx
+++ portfolio/src/components/testimonial/Testimonial.jsx
@@ -2,11 +2,14 @@ import React from "react";
 
 function Testimonial() {
   return (
-    <div className="w-full h-[800px] pt-20 pb-20 border-b-[1px] border-b-black p-4">
+    <section
+      id="testimonial"
+      className="w-full h-[800px] pt-20 pb-20 border-b-[1px] border-b-black p-4"
+    >
       <div className="flex justify-center items-center text-center">
         <Title title="What Clients Say" des="Testimonials" />
       </div>
-    </div>
+    </section>
   );
 }
 
