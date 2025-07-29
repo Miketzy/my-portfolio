90ec68a12b44c6adff1fecf0a98da412302d2e23
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-07-29 17:00:55 +0800

    updated

--- portfolio/src/components/resume/Experience.jsx
+++ portfolio/src/components/resume/Experience.jsx
@@ -5,7 +5,7 @@ function Experience() {
   return (
     <div className="flex flex-col w-full lg:w-[700px]">
       <div className="flex flex-col gap-4">
-        <h1 className="text-4xl text-[#85DFE7] font-bold">Experience</h1>
+        <h1 className="text-4xl text-[#85DFE7] font-bold">My Experience</h1>
         <p className="text-base leading-relax text-white mb-6">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus
           similique molestiae magnam facilis vero deserunt ratione delectus
