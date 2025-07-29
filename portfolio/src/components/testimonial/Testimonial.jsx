385a29b3b95f6ede72cf5ddf39153fa4f935c413
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-07-29 17:33:52 +0800

    updated

---
+++ portfolio/src/components/testimonial/Testimonial.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Testimonial() {
+  return (
+    <div className="w-full h-[800px] pt-20 pb-20 border-b-[1px] border-b-black p-4">
+      <div className="flex justify-center items-center text-center">
+        <Title title="What Clients Say" des="Testimonials" />
+      </div>
+    </div>
+  );
+}
+
+export default Testimonial;
