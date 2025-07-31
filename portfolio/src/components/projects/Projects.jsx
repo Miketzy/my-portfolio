4c4391a9e19141b23b3ed0b0e0b49deaffc9f9d6
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-07-31 18:37:51 +0800

    updated

--- portfolio/src/components/projects/Projects.jsx
+++ portfolio/src/components/projects/Projects.jsx
@@ -16,8 +16,8 @@ function Projects() {
       </div>
       <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-10">
         <ProjectCard
-          title="Title"
-          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia assumenda ratione cumque iusto ducimus, magni optio tempore harum reiciendis natus quis eum laudantium consectetur aperiam sit doloremque repellendus quam exercitationem?"
+          title="BioExplorer: A Web-Based Reposatory of Davao Oriental Species."
+          des="This website is made to help people discover and learn about the different animal species found in Davao Oriental. It provides helpful information and images of local wildlife in one easy-to-use place. BioExplorer supports awareness and education about the unique animals in the region."
           src={projectOne}
         />
         <ProjectCard
