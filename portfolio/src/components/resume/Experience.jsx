03bd9e2d31d5b5f784cead4c8d3ec8392212aa35
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-08-01 16:39:19 +0800

    updated

--- portfolio/src/components/resume/Experience.jsx
+++ portfolio/src/components/resume/Experience.jsx
@@ -18,7 +18,7 @@ function Experience() {
       <div className="grid md:grid-cols-2 lg:grid-cols-2 gap-5 ">
         <ExperieceCard
           title="Full Stack Developer"
-          tools="Reactjs, Tailwindcss,Express.js,Postgresql"
+          tools="ReactJS, Tailwind CSS, Express.js, PostgreSQL"
           des={
             <>
               <ul className="list-disc list-inside text-sm">
