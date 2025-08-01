72240679dba1448fa83fed4d4c54888eb171ef63
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-08-01 15:53:21 +0800

    updated

--- portfolio/src/components/testimonial/Testimonial.jsx
+++ portfolio/src/components/testimonial/Testimonial.jsx
@@ -6,7 +6,7 @@ function Testimonial() {
   return (
     <section
       id="testimonial"
-      className="w-full pt-20 pb-20 border-b-[1px] border-b-black p-4"
+      className="w-full h-[600px] pt-20 pb-20 border-b-[1px] border-b-black p-4"
     >
       <div className="flex justify-center items-center text-center">
         <Title title="What Clients Say" des="Testimonials" />
