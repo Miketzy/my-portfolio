0161e41ed0b596366e3723de1e9b74dde50cc611
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-08-01 15:34:57 +0800

    updated

--- portfolio/src/components/testimonial/TestimonilasCard.jsx
+++ portfolio/src/components/testimonial/TestimonilasCard.jsx
@@ -3,20 +3,20 @@ import React from "react";
 function TestimonilasCard({ image, name, des }) {
   return (
     <div className="w-full px-12 h-auto py-10 rounded-lg bg-gradient-to-r from-gray-800 to-gray-900 shadow-[10px_10px_20px_rgba(0,0,0,0.5),_-10px_-10px_20px_rgba(255,255,255,0.05)] hover:bg-gradient-to-b hover:from-gray-700 hover:to-gray-800 transition-colors duration-300 group">
-      <div classname="w-full h-[80%] overflow-hidden rounded-lg block mx-auto">
+      {/* Center the image */}
+      <div className="w-full flex justify-center">
         <img
-          className="w-30 h-30 object-cover rounded-full "
+          className="w-24 h-24 object-cover rounded-full"
           src={image}
-          alt="src"
+          alt="Profile"
         />
       </div>
-      <div className="w-full mt-5 flex flex-col gap-6">
+
+      <div className="w-full mt-5 flex flex-col gap-6 text-center">
         <div>
-          <div className="flex items-center justify-between">
-            <h3 className="text-base uppercase text-[#85DFE7] font-normal">
-              {name}
-            </h3>
-          </div>
+          <h3 className="text-base uppercase text-[#85DFE7] font-normal">
+            {name}
+          </h3>
           <p className="text-sm tracking-wide mt-3 hover:text-[#85DFE7] duration-300">
             {des}
           </p>
