61aaac9cab9a384207985bffa6ee59b518ec0d48
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-08-01 16:26:41 +0800

    updated

--- portfolio/src/components/resume/ExperieceCard.jsx
+++ portfolio/src/components/resume/ExperieceCard.jsx
@@ -4,7 +4,7 @@ function ExperieceCard({ title, des, tools }) {
   return (
     <div className="bg-[#1f2937] p-5 rounded-lg shadow-md text-white cursor-pointer hover:bg-[#374151] transition">
       <h2 className="text-xl font-semibold text-white">{title}</h2>
-      <h4 className="text-based text-gray-300 mt-2 text-center">{tools}</h4>
+      <h4 className="text-based text-gray-300 mt-2">{tools}</h4>
       <p className="text-sm text-gray-300 mt-2">{des}</p>
     </div>
   );
