524a799c4a99d98fdb34466ef77f572be7715a1c
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-08-01 15:29:58 +0800

    updated

--- portfolio/src/components/testimonial/TestimonilasCard.jsx
+++ portfolio/src/components/testimonial/TestimonilasCard.jsx
@@ -3,9 +3,9 @@ import React from "react";
 function TestimonilasCard({ image, name, des }) {
   return (
     <div className="w-full px-12 h-auto py-10 rounded-lg bg-gradient-to-r from-gray-800 to-gray-900 shadow-[10px_10px_20px_rgba(0,0,0,0.5),_-10px_-10px_20px_rgba(255,255,255,0.05)] hover:bg-gradient-to-b hover:from-gray-700 hover:to-gray-800 transition-colors duration-300 group">
-      <div classname="w-full h-[80%] overflow-hidden rounded-lg items-center justify-center">
+      <div classname="w-full h-[80%] overflow-hidden rounded-lg ">
         <img
-          className="w-30 h-30 object-cover rounded-full"
+          className="w-30 h-30 object-cover rounded-full items-center justify-center"
           src={image}
           alt="src"
         />
