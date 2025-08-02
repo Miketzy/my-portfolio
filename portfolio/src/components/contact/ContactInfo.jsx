726ba0915e52e30ad0e44c488b377d1a1c93a9ca
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-08-02 17:12:09 +0800

    updated

--- portfolio/src/components/contact/ContactInfo.jsx
+++ portfolio/src/components/contact/ContactInfo.jsx
@@ -3,8 +3,8 @@ import React from "react";
 function ContactInfo() {
   return (
     <div className="flex flex-col">
-      <div className="bg-blue-200 w-full h-50">Set 1</div>
-      <div className="bg-blue-600 w-full h-50">Set 2</div>
+      <div className="bg-blue-200 w-full h-[50%]">Set 1</div>
+      <div className="bg-blue-600 w-full h-[50%]">Set 2</div>
     </div>
   );
 }
