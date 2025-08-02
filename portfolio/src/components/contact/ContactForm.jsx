d67d05d9b4f416f937d2258b8aa392b369a00154
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-08-02 17:16:27 +0800

    updated

--- portfolio/src/components/contact/ContactForm.jsx
+++ portfolio/src/components/contact/ContactForm.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function ContactForm() {
-  return <div className="bg-red-500 w-[50%] h-[100%]">ContactForm</div>;
+  return <div className="bg-red-500 w-[50%] h-full">ContactForm</div>;
 }
 
 export default ContactForm;
--- portfolio/src/components/contact/ContactInfo.jsx
+++ portfolio/src/components/contact/ContactInfo.jsx
@@ -2,7 +2,9 @@ import React from "react";
 
 function ContactInfo() {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col h-96">
+      {" "}
+      {/* ← fixed height */}
       <div className="bg-blue-200 w-full h-[50%]">Set 1</div>
       <div className="bg-blue-600 w-full h-[50%]">Set 2</div>
     </div>
