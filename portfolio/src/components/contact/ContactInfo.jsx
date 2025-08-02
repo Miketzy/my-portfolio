d0c232e74f716a90a39d51732cab1cccadf322c8
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-08-02 17:21:30 +0800

    updated

--- portfolio/src/components/contact/ContactInfo.jsx
+++ portfolio/src/components/contact/ContactInfo.jsx
@@ -5,8 +5,8 @@ function ContactInfo() {
     <div className="flex flex-col h-96 gap-10">
       {" "}
       {/* ← fixed height */}
-      <div className="bg-blue-200 w-[50%] h-[50%]">Set 1</div>
-      <div className="bg-blue-600 w-[50%] h-[50%]">Set 2</div>
+      <div className="bg-blue-200 h-[50%]">Set 1</div>
+      <div className="bg-blue-600 h-[50%]">Set 2</div>
     </div>
   );
 }
