97a88c9cb73287185a9d2ee40fd81151c15353e8
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-08-02 10:38:02 +0800

    updated

---
+++ portfolio/src/components/contact/Contact.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Contact() {
+  return (
+    <section
+      id="contacts"
+      className="w-full  pt-20 pb-20 border-b-[1px] border-b-black p-4"
+    >
+      <Title title="Contact" des="Contact With Me" />
+    </section>
+  );
+}
+
+export default Contact;
