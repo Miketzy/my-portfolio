c2da48af63e298dba4c40505aa3830409248d17c
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-08-03 07:10:39 +0800

    updated

--- portfolio/src/components/contact/ContactForm.jsx
+++ portfolio/src/components/contact/ContactForm.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function ContactForm() {
   return (
-    <div className="flex flex-col w-full px-6 py-10 bg-gradient-to-r from-gray-800 to-gray-900 shadow-[10px_10px_20px_rgba(0,0,0,0.5),_-10px_-10px_20px_rgba(255,255,255,0.05)] group rounded-lg text-white">
+    <div className="flex flex-col w-full px-6 py-10 lg:px-12  bg-gradient-to-r from-gray-800 to-gray-900 shadow-[10px_10px_20px_rgba(0,0,0,0.5),_-10px_-10px_20px_rgba(255,255,255,0.05)] group rounded-lg text-white">
       <div className="flex flex-col lg:flex-row gap-4 mb-4">
         <div className="w-full">
           <label className="block mb-1 text-sm">First Name</label>
