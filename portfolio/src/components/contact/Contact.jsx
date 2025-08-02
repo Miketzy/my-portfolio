60ecd26e9e64c52b41c3027fee4485597c12b97d
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-08-02 17:08:04 +0800

    updated

--- portfolio/src/components/contact/Contact.jsx
+++ portfolio/src/components/contact/Contact.jsx
@@ -13,7 +13,7 @@ function Contact() {
         <Title title="Contact" des="Contact With Me" />
       </div>
 
-      <div className="flex flex-col lg:flex-row gap-10 w-full h-50">
+      <div className="flex flex-col lg:flex-row gap-10 w-full h-full">
         <ContactForm />
         <ContactInfo />
       </div>
