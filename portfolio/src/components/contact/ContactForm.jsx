7ab8eec02922b6aed858a5d7537db98c3afe2f22
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-08-03 06:28:27 +0800

    updated

--- portfolio/src/components/contact/ContactForm.jsx
+++ portfolio/src/components/contact/ContactForm.jsx
@@ -2,17 +2,29 @@ import React from "react";
 
 function ContactForm() {
   return (
-    <div className="flex flex-col px-12 py-10">
-      <div className="flex flex-col lg:flex-row">
-        <input type="text" />
-        <input type="text" />
+    <div className="bg-gray-900 flex flex-col px-12 py-10 shadow-lg rounded-lg">
+      <div className="flex flex-col lg:flex-row gap-4 mb-4">
+        <input
+          type="text"
+          className="w-full p-2 rounded"
+          placeholder="First Name"
+        />
+        <input
+          type="text"
+          className="w-full p-2 rounded"
+          placeholder="Last Name"
+        />
       </div>
-      <div className="flex flex-col lg:flex-row">
-        <input type="text" />
-        <input type="text" />
+      <div className="flex flex-col lg:flex-row gap-4 mb-4">
+        <input type="text" className="w-full p-2 rounded" placeholder="Email" />
+        <input type="text" className="w-full p-2 rounded" placeholder="Phone" />
       </div>
-      <div className="">
-        <textarea name="" id=""></textarea>
+      <div>
+        <textarea
+          className="w-full p-2 rounded resize-none"
+          rows="5"
+          placeholder="Your Message"
+        ></textarea>
       </div>
     </div>
   );
