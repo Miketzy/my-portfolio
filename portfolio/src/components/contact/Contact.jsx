e6e061b044742cc28f5a4e9aa4504be33ef73e51
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-08-02 10:43:08 +0800

    updated

--- portfolio/src/components/contact/Contact.jsx
+++ portfolio/src/components/contact/Contact.jsx
@@ -7,7 +7,9 @@ function Contact() {
       id="contacts"
       className="w-full h-[800px] pt-20 pb-20 border-b-[1px] border-b-black p-4"
     >
-      <Title title="Contact" des="Contact With Me" />
+      <div className="flex justify-center items-center text-center">
+        <Title title="Contact" des="Contact With Me" />
+      </div>
     </section>
   );
 }
