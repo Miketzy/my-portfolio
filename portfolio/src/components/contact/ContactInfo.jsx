5f1966e052eb3dfb6e77167d3e764167e98defea
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-08-03 06:44:04 +0800

    updated

--- portfolio/src/components/contact/ContactInfo.jsx
+++ portfolio/src/components/contact/ContactInfo.jsx
@@ -5,13 +5,13 @@ function ContactInfo() {
     <div className="flex flex-col w-[30%] h-full gap-10">
       {" "}
       {/* ← fixed height */}
-      <div className="bg-blue-200 px-10 py-12 ">
+      <div className="flex flex-col px-12 py-10 bg-gradient-to-r from-gray-800 to-gray-900 shadow-[10px_10px_20px_rgba(0,0,0,0.5),_-10px_-10px_20px_rgba(255,255,255,0.05)] group rounded-lg text-white">
         <h1>set 1</h1>
         <h1>set 1</h1>
         <h1>set 1</h1>
         <h1>set 1</h1>
       </div>
-      <div className="bg-blue-600 ">
+      <div className="flex flex-col  bg-gradient-to-r from-gray-800 to-gray-900 shadow-[10px_10px_20px_rgba(0,0,0,0.5),_-10px_-10px_20px_rgba(255,255,255,0.05)] group rounded-lg text-white ">
         <h1>set 1</h1>
         <h1>set 1</h1>
         <h1>set 1</h1>
