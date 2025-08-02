49e4326a480054b7f4ef508a918bd96cf5537023
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-08-02 17:14:03 +0800

    updated

--- portfolio/src/components/contact/ContactForm.jsx
+++ portfolio/src/components/contact/ContactForm.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function ContactForm() {
-  return <div className="bg-red-500 w-full h-full">ContactForm</div>;
+  return <div className="bg-red-500 w-[50%] h-[100%]">ContactForm</div>;
 }
 
 export default ContactForm;
