798cac548f8790dfd87dcc7c7e137990dcf3a84d
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-08-07 07:07:14 +0800

    updated

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -25,7 +25,12 @@ function App() {
     return (
       <div className="loadingContainer">
         <div className="spinner relative flex items-center justify-center">
-          <img src={logo} alt="Logo" className="absolute w-10 h-10" />
+          <img
+            src={logo}
+            alt="Logo"
+            className="absolute w-10 h-10"
+            style={{ animation: "none" }}
+          />
         </div>
         <p>Loading website...</p>
       </div>
